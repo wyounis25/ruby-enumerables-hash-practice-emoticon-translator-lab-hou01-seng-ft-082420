45fda596ba11cddd26ac412f63fab9cd07926414
walid younis <walid.younis.90@gmail.com>
2020-08-23 18:10:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,20 @@
-# require modules here
+require "yaml"
+
 
 def load_library
-  # code goes here
+  
+  
+    emotions = YAML.load_file('emoticons.yml')
+   
+    
 end
 
 def get_japanese_emoticon
   # code goes here
 end
+
+
+
 
 def get_english_meaning
   # code goes here
